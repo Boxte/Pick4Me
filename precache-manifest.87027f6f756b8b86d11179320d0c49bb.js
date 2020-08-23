1b763fa2bb1386b6ab000fcce82ebef0ab65ad0b
Van <van@vans-mac-mini.lan>
2020-08-23 18:14:41 -0400

    Updates

---
+++ precache-manifest.87027f6f756b8b86d11179320d0c49bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33d74a5a87c70825305926eac005f902",
+    "url": "/pick4me/index.html"
+  },
+  {
+    "revision": "0001ac93d770856fa690",
+    "url": "/pick4me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1afe1de52a9f07bb5b13",
+    "url": "/pick4me/static/js/2.5840a4cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pick4me/static/js/2.5840a4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0001ac93d770856fa690",
+    "url": "/pick4me/static/js/main.f45a9023.chunk.js"
+  },
+  {
+    "revision": "c3151f23ecda531eb275",
+    "url": "/pick4me/static/js/runtime-main.3dabce31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pick4me/static/media/logo.5d5d9eef.svg"
+  }
+]);

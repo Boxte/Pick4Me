fa7cd0e0f5873b5409d8169aa90f74698bcab2c1
Van <van@vans-mac-mini.lan>
2020-08-28 01:07:12 -0400

    Updates

--- precache-manifest.48d73ae07c55bbc28dcb8a759675ed41.js
+++ precache-manifest.48d73ae07c55bbc28dcb8a759675ed41.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b7ed69de39c94128bb77008cb8e73329",
+    "revision": "107fce657585b394150074d33ba3fad1",
     "url": "/Pick4Me/index.html"
   },
   {
-    "revision": "6d2766bf91e5b2af145f",
+    "revision": "562e233f037a4b5e1965",
     "url": "/Pick4Me/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "1afe1de52a9f07bb5b13",
-    "url": "/Pick4Me/static/js/2.5840a4cf.chunk.js"
+    "revision": "7bcc9cf10fbd47d78e48",
+    "url": "/Pick4Me/static/js/2.85afb29b.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/Pick4Me/static/js/2.5840a4cf.chunk.js.LICENSE.txt"
+    "url": "/Pick4Me/static/js/2.85afb29b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6d2766bf91e5b2af145f",
-    "url": "/Pick4Me/static/js/main.81502473.chunk.js"
+    "revision": "562e233f037a4b5e1965",
+    "url": "/Pick4Me/static/js/main.db2d154f.chunk.js"
   },
   {
     "revision": "e54fd2b95d340b47fa1c",

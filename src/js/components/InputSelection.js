035a762018a0057697e42686a55d6080e5eadf1f
Van <van@vans-mac-mini.lan>
2020-08-29 03:59:52 -0400

    Added input selection buttons

--- src/js/components/InputSelection.js
+++ src/js/components/InputSelection.js
@@ -1,10 +1,22 @@
 import React from "react";
+import classNames from "classnames";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMicrophone, faKeyboard } from "@fortawesome/free-solid-svg-icons";
+
+import "./InputSelection.css";
 
 export const InputSelection = (props) => {
   return (
-    <div>
-      <button>VOICE</button>
-      <button>Text</button>
+    <div className="internal-input-selection-holder">
+      <button className={classNames("voice-button", "input-selection-button")}>
+        <FontAwesomeIcon icon={faMicrophone} />
+        <span className="input-selection-button-text">Voice</span>
+      </button>
+      <button className={classNames("text-button", "input-selection-button")}>
+        <FontAwesomeIcon icon={faKeyboard} />
+        <span className="input-selection-button-text">Text</span>
+      </button>
     </div>
   );
 };

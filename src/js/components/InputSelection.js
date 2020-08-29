529b45d2fbf40c46611884d0a43cb596011e23bc
Van <van@vans-mac-mini.lan>
2020-08-29 04:08:18 -0400

    Added title for input selection

--- src/js/components/InputSelection.js
+++ src/js/components/InputSelection.js
@@ -9,14 +9,19 @@ import "./InputSelection.css";
 export const InputSelection = (props) => {
   return (
     <div className="internal-input-selection-holder">
-      <button className={classNames("voice-button", "input-selection-button")}>
-        <FontAwesomeIcon icon={faMicrophone} />
-        <span className="input-selection-button-text">Voice</span>
-      </button>
-      <button className={classNames("text-button", "input-selection-button")}>
-        <FontAwesomeIcon icon={faKeyboard} />
-        <span className="input-selection-button-text">Text</span>
-      </button>
+      <p className="input-selection-title">Talk to Mr. Wit using...</p>
+      <div className="internal-input-selection-options-holder">
+        <button
+          className={classNames("voice-button", "input-selection-button")}
+        >
+          <FontAwesomeIcon icon={faMicrophone} />
+          <span className="input-selection-button-text">Voice</span>
+        </button>
+        <button className={classNames("text-button", "input-selection-button")}>
+          <FontAwesomeIcon icon={faKeyboard} />
+          <span className="input-selection-button-text">Text</span>
+        </button>
+      </div>
     </div>
   );
 };

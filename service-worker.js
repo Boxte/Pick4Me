dd35015595ea3345dcd86a23ed4be7d9ee6f2f60
Van <van@vans-mac-mini.lan>
2020-09-07 20:20:44 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Pick4Me/precache-manifest.0c2ec31ddc2015eb3b119bcdd6e48924.js"
+  "/Pick4Me/precache-manifest.c34a25455e982a5a67971dba10d0bc63.js"
 );
 
 self.addEventListener('message', (event) => {
